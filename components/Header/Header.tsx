c6b68f29795b90d427d10ac00474f3dd9d98c78d
Yaroslav <81682837+Yarkaruf@users.noreply.github.com>
2022-10-15 11:21:49 +0800

    test fetch

---
+++ components/Header/Header.tsx
@@ -0,0 +1,14 @@
+export const Header = (): JSX.Element => {
+	return (
+		<header>
+			<nav>
+				<ul>
+					<li>Персонажи</li>
+					<li>Локации</li>
+					<li>Эпизоды</li>
+					<li>Настройки</li>
+				</ul>
+			</nav>
+		</header>
+	)
+}
